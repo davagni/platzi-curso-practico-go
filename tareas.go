fdf21576f862944a4b593c3e843160dff811e23a
Vangi <dvangioni@gmail.com>
2021-02-12 22:06:12 -0300

    Uso de Slices

--- tareas.go
+++ tareas.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func (tl *taskList) agregarALista(t *task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *taskList) eliminarDeLista(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -21,16 +33,34 @@ func (t *task) actualizarNombre(nombre string) {
 }
 
 func main() {
-	t := &task{
+	t1 := &task{
 		nombre:      "Completar mi curso de Go",
 		descripcion: "Completar mi curso de Go de Platzi en esta semana",
 	}
 
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		nombre:      "Completar mi curso de Python",
+		descripcion: "Completar mi curso de Python de Platzi en esta semana",
+	}
 
-	t.marcarCompleta()
-	t.actualizarDescripcion("Finalizar mi curso de Go")
-	t.actualizarNombre("Completar mi curso cuanto antes")
+	t3 := &task{
+		nombre:      "Completar mi curso de NodeJS",
+		descripcion: "Completar mi curso de NodeJS de Platzi en esta semana",
+	}
 
-	fmt.Printf("%+v\n", t)
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+
+	fmt.Println(lista.tasks[0])
+
+	lista.agregarALista(t3)
+
+	fmt.Println(len(lista.tasks))
+
+	lista.eliminarDeLista(1)
+
+	fmt.Println(len(lista.tasks))
 }
