f4194aebe2551ae3bc574dcd25bfa5a905ff5748
Vangi <dvangioni@gmail.com>
2021-02-18 22:06:21 -0300

    Manejando modelos y JSON

--- web_server/handlers.go
+++ web_server/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,4 +14,17 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 //HandleHome a
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the API Endpoint")
+}
+
+//PostRequest a
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metaData MetaData
+	err := decoder.Decode(&metaData)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Payload: %v\n", metaData)
 }
--- web_server/types.go
+++ web_server/types.go
@@ -4,3 +4,13 @@ import "net/http"
 
 //Middleware a
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+//User a
+type User struct {
+	Name  string
+	Email string
+	Phone string
+}
+
+//MetaData a
+type MetaData interface{}
